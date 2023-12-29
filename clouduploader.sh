6ca59246f884cae18438508c21a6a1c34e6b5f73
Luqmaan Hakeem <rockgameplayhakeem@gmail.com>
2023-12-29 17:20:03 +0530

    code: Added more changes and automated file upload

--- clouduploader.sh
+++ clouduploader.sh
@@ -3,24 +3,25 @@ Help()
 {
    echo "A tool to Upload your files to Azure Blob"
    echo
-   echo "Syntax: clouduploader upload [-n]"
-   echo "Note: Login using the below before uploading files"
-   echo "Sytanx: clouduploader login"
-   echo "options:"
-   echo "-n  <Storage Name>"
-   echo "-c <Connection String>"
+   echo "Syntax For File Upload: clouduploader <filepath> <container-name>"
+   echo "Syntax For Directory Upload: clouduploader <DirPath>/* <container-name>"
+   echo "Note: Store the connection string in the AZURE_STORAGE_CONNECTION_STRING env variable"
    echo
 }
 
-if [ "$1" == "login" ]; then
-az login;
-elif [ "$1" == "upload" ]; then
-	read -p "Enter the path:" path;
-	if [ -f $path ]; then
-		echo "uploading"
-	else echo "File not Found.....Please enter a valid file path";
-	fi 
-fi
+Upload(){
+   pv "$1" | az storage azcopy blob upload -c "$2" --connection-string $AZURE_STORAGE_CONNECTION_STRING -s "$1" --recursive &> output.txt;
+   if  tail -n 2 output.txt | grep "Completed"; then
+   echo "File Upload Success :-)"
+   else
+   echo "--------------------------------------------------FILE UPLOAD FAILED--------------------------------------------------"
+   echo
+   echo "-------------------------------------------------------------Error:-------------------------------------------------------------";
+   cat output.txt;
+   fi
+
+}
+
 
 while getopts ":h" flag; do
    case $flag in
@@ -32,3 +33,16 @@ while getopts ":h" flag; do
          exit;;
    esac
 done
+
+if az account list &> output.txt && grep "login" output.txt; then
+az login &> output.txt;
+fi
+
+if [ -f "$1" ]; then
+Upload "$1" "$2";
+elif [ -d "$1" ]; then
+Upload "$1" "$2";
+else echo "File does not exist...Please enter a valid path";
+fi
+
+
